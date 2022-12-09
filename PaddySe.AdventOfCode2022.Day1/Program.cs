596f1914e90c5cd7fa49c5652d97bb0058291265
Patrik Berglund <patrik.berglund@egmont.se>
2022-12-09 13:29:34 +0100

    Finished day 1.

---
+++ PaddySe.AdventOfCode2022.Day1/Program.cs
@@ -0,0 +1,29 @@
+﻿var elfCounter = 0;
+var calorieCount = 0;
+var elfs = new Dictionary<int, int>();
+
+using var fileStream = File.Open("Input.txt", FileMode.Open);
+using var reader = new StreamReader(fileStream);
+
+while (!reader.EndOfStream)
+{
+	var line = reader.ReadLine();
+	if (int.TryParse(line, out var calories))
+	{
+		calorieCount += calories;
+	}
+	else
+	{
+		elfs.Add(elfCounter, calorieCount);
+		elfCounter++;
+		calorieCount = 0;
+	}
+}
+
+// Part 1
+var elfWithMostCalories = elfs.MaxBy(item => item.Value);
+Console.WriteLine($"[PART 1] The Elf with most calories is {elfWithMostCalories.Key + 1}, and they have {elfWithMostCalories.Value} calories.");
+
+// Part 2
+var topThreeCalories = elfs.OrderByDescending(item => item.Value).Take(3).Sum(item => item.Value);
+Console.WriteLine($"[PART 2] The top three Elves carry a total of {topThreeCalories} calories.");
