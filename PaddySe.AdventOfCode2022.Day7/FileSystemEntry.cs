f289079ad5e3769558e6a07dc01bf389dee571af
Patrik Berglund <patrik.berglund@gmail.com>
2022-12-10 21:52:03 +0100

    Finished day 7.

---
+++ PaddySe.AdventOfCode2022.Day7/FileSystemEntry.cs
@@ -0,0 +1,80 @@
+﻿namespace PaddySe.AdventOfCode2022.Day7;
+
+public class FileSystemEntry
+{
+	private readonly List<FileSystemEntry> _entries = new();
+	private readonly int _size;
+
+	private FileSystemEntry(string name, int size, FileSystemEntry? parent)
+	{
+		Name = name;
+		_size = size;
+		IsDirectory = size == 0;
+		Parent = parent ?? this;
+	}
+
+	public bool IsDirectory { get; }
+
+	public string Name { get; }
+
+	public FileSystemEntry Parent { get; }
+
+	public int Size => _size + _entries.Sum(item => item.Size);
+
+	public IEnumerable<FileSystemEntry> AllDirectories()
+	{
+		var directories = new List<FileSystemEntry>();
+		if (IsDirectory && Name != "/")
+		{
+			directories.Add(this);
+		}
+
+		directories.AddRange(_entries.Where(item => item.IsDirectory).SelectMany(item => item.AllDirectories()));
+
+		return directories;
+	}
+
+	public static FileSystemEntry CreateRoot() => new("/", 0, null);
+
+	public void ParseAndAdd(string line)
+	{
+		var split = line.Split(' ');
+		_entries.Add(new(split[1], split[0] == "dir" ? 0 : int.Parse(split[0]), this));
+	}
+
+	public static FileSystemEntry ParseDirectoryStructure(IList<string> input)
+	{
+		var root = CreateRoot();
+		var currentFolder = root;
+
+		for (var i = 0; i < input.Count; i++)
+		{
+			var split = input[i].Split(' ');
+			switch (split[1])
+			{
+				case "cd":
+					currentFolder = split[2] switch
+					{
+						"/" => root,
+						".." => currentFolder.Parent,
+						_ => currentFolder._entries.First(item => item.Name == split[2])
+					};
+					break;
+				case "ls":
+					{
+						foreach (var entry in input.Skip(i + 1).TakeWhile(item => item[0] != '$'))
+						{
+							currentFolder.ParseAndAdd(entry);
+							i++;
+						}
+
+						break;
+					}
+			}
+		}
+
+		return root;
+	}
+
+	public override string ToString() => IsDirectory ? $"{Name} (dir, size={Size})" : $"{Name} (file, size={_size}";
+}
---
+++ PaddySe.AdventOfCode2022.Day7/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using PaddySe.AdventOfCode2022.Day7;
+
+Validate();
+
+var sampleInput = File.ReadAllLines("Input.txt");
+var root = FileSystemEntry.ParseDirectoryStructure(sampleInput);
+
+/* ----- Part 1 ----- */
+
+var directories = root.AllDirectories().Where(item => item.Size < 100000).ToList();
+var sum = directories.Sum(item => item.Size);
+Console.WriteLine($"[PART 1] Sum size: {sum}");
+
+
+/* ----- Part 2 ----- */
+
+const int totalSize = 70_000_000;
+const int neededSpace = 30_000_000;
+var freeSpace = totalSize - root.Size;
+
+var candidate = root.AllDirectories().Where(item => item.Size + freeSpace >= neededSpace).OrderBy(item => item.Size).First();
+
+Console.WriteLine($"[PART 2] Folder to delete: {candidate}");
+
+
+static void Validate()
+{
+	var input = new List<string>
+	{
+		"$ cd /",
+		"$ ls",
+		"dir a",
+		"14848514 b.txt",
+		"8504156 c.dat",
+		"dir d",
+		"$ cd a",
+		"$ ls",
+		"dir e",
+		"29116 f",
+		"2557 g",
+		"62596 h.lst",
+		"$ cd e",
+		"$ ls",
+		"584 i",
+		"$ cd ..",
+		"$ cd ..",
+		"$ cd d",
+		"$ ls",
+		"4060174 j",
+		"8033020 d.log",
+		"5626152 d.ext",
+		"7214296 k"
+	};
+
+	var root = FileSystemEntry.ParseDirectoryStructure(input);
+	var directories = root.AllDirectories().Where(item => item.Size < 100000).ToList();
+	var sum = directories.Sum(item => item.Size);
+	Debug.Assert(sum == 95437, "Incorrect sum!");
+	Console.WriteLine($"[VALIDATION 1] Sum size: {sum}");
+
+	const int totalSize = 70_000_000;
+	const int neededSpace = 30_000_000;
+	var freeSpace = totalSize - root.Size;
+	var candidate = root.AllDirectories().Where(item => item.Size + freeSpace >= neededSpace).OrderBy(item => item.Size).First();
+
+	Debug.Assert(candidate.Name == "d", "Wrong folder to delete");
+	Console.WriteLine($"[VALIDATION 2] Folder to delete: {candidate}");
+}
